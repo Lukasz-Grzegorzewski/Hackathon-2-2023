0dbb85769431a3f9a27cae4809c900c25f9da24f
Edouard de Champeaux <bobdechampeaux@gmail.com>
2023-01-11 15:55:51 +0100

    new component vehicule created

--- frontend/src/components/Vehicules.jsx
+++ frontend/src/components/Vehicules.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Vehicules() {
-  return <button>Voir les véhicules disponible</button>;
+  return <button type="button">Voir les véhicules disponible</button>;
 }
 
 export default Vehicules;
