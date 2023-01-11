21ee9149039a4c8f1e1d0cdb675b53e7e5eb45f5
Jonathan Scattolini <jscattolini@gmail.com>
2023-01-11 16:00:18 +0100

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Search from "@components/Search";
+import Vehicules from "@components/Vehicules";
 
 function Home() {
   return (
     <div>
       <Search />
+      <Vehicules />
     </div>
   );
 }
