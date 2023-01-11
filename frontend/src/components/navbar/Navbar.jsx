2b799795c87dff95a7bc6419625ae95c6da1b6f1
Jonathan Scattolini <jscattolini@gmail.com>
2023-01-11 23:53:10 +0100

    :lipstick: Update CSS Add Variables Btn for free

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
 function Navbar() {
@@ -7,6 +8,38 @@ function Navbar() {
       <div className="navbar_logo">
         <img className="navbar_logo_img" src={logo} alt="logo" />
         <p className="navbar_logo_text">Let's Go !</p>
+      </div>
+      <div className="multi-button">
+        <Link
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          to="/"
+        >
+          <button type="button" className="multi-button_btn1">
+            Home
+          </button>
+        </Link>
+        <Link
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          to="/"
+        >
+          <button type="button" className="multi-button_btn2">
+            Admin
+          </button>
+        </Link>
+        <Link
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          to="/"
+        >
+          <button type="button" className="multi-button_btn3">
+            Race
+          </button>
+        </Link>
       </div>
     </div>
   );
