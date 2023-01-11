c922342c52cd56cf6a822aaf16b91853921b9e56
Lukasz Grzegorzewski <lukasz@macbook-pro-lukasz.home>
2023-01-11 15:55:52 +0100

    Lukasz getVehicules and getVehiculeById done

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,17 +5,6 @@ const router = express.Router();
 const vehiculesControllers = require("./controllers/vehicules.controller");
 
 router.get("/vehicules", vehiculesControllers.getVehicules);
-// router.get("/vehicules/:id", vehiculesControllers.read);
-// router.put("/vehicules/:id", vehiculesControllers.edit);
-// router.post("/vehicules", vehiculesControllers.add);
-// router.delete("/vehicules/:id", vehiculesControllers.destroy);
+router.get("/vehicules/:id", vehiculesControllers.getVehiculeById);
 
 module.exports = router;
-
-// const itemControllers = require("./controllers/itemControllers");
-// const vehiculesControllers = require("./controllers/vehiculeControllers");
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
