ca63f13ecb8fca5f427587b5129e63cdf106319f
Jonathan Scattolini <jscattolini@gmail.com>
2023-01-12 19:18:07 +0100

    post vehicule ok

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import FleetPost from "@components/admin_fleet/FleetPost";
-// import FleetUpdate from "@components/admin_fleet/FleetUpdate";
-// import FleetDelete from "@components/admin_fleet/FleetDelete";
+import FleetUpdate from "@components/admin_fleet/FleetUpdate";
+import FleetDelete from "@components/admin_fleet/FleetDelete";
 
 function Admin() {
-  // const [vehicules, setVehicules] = useState([]);
+  const [vehicules, setVehicules] = useState([]);
 
   function getVehicules() {
     axios
       .get(`${import.meta.env.VITE_PORT_BACKEND}/vehicules`)
       .then((res) => {
-        // setVehicules(res.data);
+        setVehicules(res.data);
         console.warn(res.data);
       })
       .catch((err) => console.error(err));
@@ -22,9 +22,28 @@ function Admin() {
 
   return (
     <div className="admin">
-      <FleetPost getVehicules={() => getVehicules()} />
-      {/* <FleetUpdate vehicules={vehicules} getVehicules={getVehicules} />
-      <FleetDelete vehicules={vehicules} getVehicules={getVehicules} /> */}
+      <div className="admin_comp">
+        <p className="admin_comp_title">Add new vehicle</p>
+        <FleetPost getVehicules={() => getVehicules()} />
+      </div>
+      {vehicules.length >= 1 && (
+        <div>
+          <div className="admin_comp">
+            <p className="admin_comp_title">Update vehicle</p>
+            <FleetUpdate
+              vehicules={vehicules}
+              getVehicules={() => getVehicules()}
+            />
+          </div>
+          <div className="admin_comp">
+            <p className="admin_comp_title">Delete vehicle</p>
+            <FleetDelete
+              vehicules={vehicules}
+              getVehicules={() => getVehicules()}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
